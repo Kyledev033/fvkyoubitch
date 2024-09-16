306fe09b0f687e101ce35cecb06693462707f1f5
Kyledev033 <kyledev40@gmail.com>
2024-09-16 16:17:48 +0800

    Update leaveall.js

--- scripts/cmds/leaveall.js
+++ scripts/cmds/leaveall.js
@@ -1,32 +1,36 @@
 module.exports = {
   config: {
     name: "leaveall",
-aliases: ["outall"],
-    version: "1.0",
-    author: "otineeey",
-    countDown: 5,
+    aliases: ["outall"],
+    version: "9.0",
+    author: "kylepogi",//don't change the author😤
+    countDown: 6,
     role: 2,
     shortDescription: {
-      vi: "",
-      en: ""
+      en: "the bot is leaving whole groups"
     },
     longDescription: {
-      vi: "",
-      en: "  "
+      en: "the bot is leaving all threads cause of the critical errors in the bot system"
     },
     category: "owner",
     guide: {
       vi: "",
-      en: ""
+      en: "example: .outall the bot automatically leave all threads with message"
     }
- },
+  },
   onStart: async function ({ api, args, message, event }) {
-    const threadList = await api.getThreadList(100, null, ["INBOX"]);
-    const botUserID = api.getCurrentUserID();
-    threadList.forEach(threadInfo => {
+    try {
+      const threadList = await api.getThreadList(100, null, ["INBOX"]);
+      const botUserID = api.getCurrentUserID();
+      for (const threadInfo of threadList) {
         if (threadInfo.isGroup && threadInfo.threadID !== event.threadID) {
+          api.sendMessage('ℹ️ 𝗭𝗘𝗣𝗛 𝗡𝗢𝗧𝗜𝗙\n\nThis bot notifies that it is leaving all threads. Goodbye, everyone!', threadInfo.threadID, () => {
             api.removeUserFromGroup(botUserID, threadInfo.threadID);
+          });
         }
-    });
-}
-}
+      }
+    } catch (error) {
+      console.error('Error while executing leaveall command:', error);
+    }
+  }
+};
